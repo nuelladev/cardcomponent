4dcd7bd75238d6d4189db934a688ac0c64585af1
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-31 12:08:29 +0100

    just a snippet

---
+++ cardcomponent/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./component/Header"
+import Footer from "./component/footer"
+import Food from "./component/Food"
+function App() {
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+   
+    </>
+  )
+}
+export default App
